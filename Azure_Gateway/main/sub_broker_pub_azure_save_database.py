937ff13b2f5abc3583a549c02dd3464e4a601bcb
AnThanh <theanuet@gmail.com>
2021-04-15 15:00:38 +0700

    pub azre ignore database

--- Azure_Gateway/main/sub_broker_pub_azure_save_database.py
+++ Azure_Gateway/main/sub_broker_pub_azure_save_database.py
@@ -3,10 +3,9 @@ import json
 import random
 from time import sleep
 from azure.iot.device import IoTHubDeviceClient, Message  
-#from publish_thingsboard import pub
 #from save_database import save
-#from publish_azure import send_azure
 
+# Azure server
 CONNECTION_STRING = "HostName=hub-anthanh.azure-devices.net;DeviceId=node;SharedAccessKey=wkA5fp+tmLyjmO1AmK+jefmZKUhYMxekjGOAJmLEXWY=" 
 MSG_SND = '{{"temperature": {temperature},"humidity": {humidity}}}' 
 
@@ -18,13 +17,13 @@ MQTT_Port = 1883
 Keep_Alive_Interval = 7200
 MQTT_Topic = 'hello'
 
-def send_azure(jsonData)
+def send_azure(jsonData):
     data = json.loads(jsonData)
     humidity = data['humidity']
     temperature = data['temperature']
     msg_txt_formatted = MSG_SND.format(temperature=temperature, humidity=humidity)
     message = Message(msg_txt_formatted)
-    print( "Sending message: {}".format(message) )
+    print( "Sending azure: {}".format(message) )
     azure.send_message(message)
     print ( "Message successfully sent" )   
 
@@ -38,7 +37,7 @@ def on_connect(client, userdata, flags, rc):
 
 # Callback on_message server
 def on_message(client, userdata, msg):
-    print("Subscribe Message Recieved: "+msg.payload.decode())
+    print("Message Broker  Recieved: "+msg.payload.decode())
     send_azure(msg.payload)
     #save(msg.payload.decode()) #save database
     #mosquitto_pub -d -t hello -m "{\"Area\":\"xuanthuy\",\"ID\":1,\"Temperature\":4,\"Humidity\":45}"
